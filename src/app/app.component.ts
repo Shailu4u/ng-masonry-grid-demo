6aace1200c8ebf2a541d7a422737ff6e6a04ad0b
Shailendra Pallikonda <Shailendra_Pallikonda@epam.com>
2018-01-24 11:49:30 +0530

    Added multiple animation effects.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <div class="container">
-    <ng-masonry-grid
+    <ul class="list">
+      <li class="list-item" *ngFor="let item of buttons">
+        <button class="btn" [ngClass]="{ 'active': item.active }" (click)="onSelect(item)">Effect {{item.index}}</button>
+      </li>
+    </ul>
+  </div>
+  <div class="container">
+    <ng-masonry-grid *ngIf="showMasonry"
                   [masonryOptions]="{ transitionDuration: '0.4s', gutter: 5 }"
                   [useAnimation]="true"
                   [useImagesLoaded]="true"
-                  [scrollAnimationOptions]="{ animationEffect: 'effect-6', minDuration : 0.4, maxDuration : 0.7 }">
+                  [scrollAnimationOptions]="animOptions">
       <ng-masonry-grid-item *ngFor="let item of masonryItems; let i = index;">
         <img src="../assets/images/{{ (i+1) >  15 ? masonryItems.length - i: i+1 }}.jpg" alt="No image" />
       </ng-masonry-grid-item>
@@ -19,9 +26,33 @@ styleUrls: ['../../node_modules/ng-masonry-grid/ng-masonry-grid.css', './app.com
 })
 export class AppComponent {
 
+  buttons = [];
+
+  showMasonry = true;
+
+  animOptions = { animationEffect: 'effect-1' };
+
   masonryItems: Array<number> = [];
 
   constructor() {
+    this.buttons = [
+      { index: 1, active: true },
+      { index: 2, active: false },
+      { index: 3, active: false },
+      { index: 4, active: false },
+      { index: 5, active: false },
+      { index: 6, active: false },
+      { index: 7, active: false },
+      { index: 8, active: false }
+    ];
     this.masonryItems.length = 30;
+  }
+
+  onSelect(item) {
+    this.showMasonry = false;
+    this.buttons.forEach( (i) => i.active = false );
+    item.active = true;
+    this.animOptions = { animationEffect: 'effect-' + item.index };
+    setTimeout(() => this.showMasonry = true, 100);
   }
 }
